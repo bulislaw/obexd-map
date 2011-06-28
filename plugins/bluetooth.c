bulislaw/obexd-map

commit f5279bfcedd669bc5d4e88cc1c59807c92226dfb

Make proper use of user_data when using bt_io_listen
This changes the user_data from server pointer to service driver pointer
since the former is not really used in the callback but the latter is.

plugins/bluetooth.c
@@ -459,7 +459,7 @@ static int request_service_authorization(struct bluetooth_service *service,
 
 static void confirm_event(GIOChannel *io, void *user_data)
 {
-	struct bluetooth_service *service;
+	struct bluetooth_service *service = user_data;
 	GError *err = NULL;
 	char address[18];
 	uint8_t channel;
@@ -476,12 +476,6 @@ static void confirm_event(GIOChannel *io, void *user_data)
 
 	info("bluetooth: New connection from: %s, channel %u", address,
 			channel);
-
-	service = find_service(NULL, channel);
-	if (service == NULL) {
-		error("bluetooth: Unable to find service");
-		goto drop;
-	}
 
 	if (service->driver->service != OBEX_OPP) {
 		if (request_service_authorization(service, io, address) < 0)
@@ -510,7 +504,7 @@ static GIOChannel *start(struct obex_server *server,
 	GError *err = NULL;
 
 	io = bt_io_listen(BT_IO_RFCOMM, NULL, confirm_event,
-				server, NULL, &err,
+				service, NULL, &err,
 				BT_IO_OPT_CHANNEL, service->channel,
 				BT_IO_OPT_SEC_LEVEL, sec_level,
 				BT_IO_OPT_INVALID);

commit 770c937872ca49f480d852995d9f03d2e457d8d8

Fix crash introduced by f5279bfcedd669bc5d4e88cc1c59807c92226dfb
The callback function actually needs the bluetooth_service structure not
obex_service_driver.

plugins/bluetooth.c
@@ -126,21 +126,15 @@ failed:
 }
 
 static struct bluetooth_service *find_service(
-					struct obex_service_driver *driver,
-					uint8_t channel)
+					struct obex_service_driver *driver)
 {
 	GSList *l;
 
 	for (l = any->services; l; l = l->next) {
 		struct bluetooth_service *service = l->data;
 
-		if (driver != NULL && service->driver != driver)
-			continue;
-
-		if (channel != 0 && service->driver->channel != channel)
-			continue;
-
-		return service;
+		if (service->driver == driver)
+			return service;
 	}
 
 	return NULL;
@@ -158,7 +152,7 @@ static void register_record(struct obex_server *server)
 		struct bluetooth_service *service;
 		char *xml;
 
-		service = find_service(driver, 0);
+		service = find_service(driver);
 		if (service == NULL) {
 			service = g_new0(struct bluetooth_service, 1);
 			service->driver = driver;
@@ -459,10 +453,11 @@ static int request_service_authorization(struct bluetooth_service *service,
 
 static void confirm_event(GIOChannel *io, void *user_data)
 {
-	struct bluetooth_service *service = user_data;
+	struct bluetooth_service *service;
 	GError *err = NULL;
 	char address[18];
 	uint8_t channel;
+	struct obex_service_driver *driver = user_data;
 
 	bt_io_get(io, BT_IO_RFCOMM, &err,
 			BT_IO_OPT_DEST, address,
@@ -477,7 +472,13 @@ static void confirm_event(GIOChannel *io, void *user_data)
 	info("bluetooth: New connection from: %s, channel %u", address,
 			channel);
 
-	if (service->driver->service != OBEX_OPP) {
+	service = find_service(driver);
+	if (service == NULL) {
+		error("bluetooth: Unable to find service");
+		goto drop;
+	}
+
+	if (driver->service != OBEX_OPP) {
 		if (request_service_authorization(service, io, address) < 0)
 			goto drop;
 