b867907aa36d92cf09bb1985d809050db2cbb0b1
Slawomir Bochenski <lkslawek@gmail.com>
2011-08-17 13:39:36 +0200

    Support for GetMessages in/out parameters

--- plugins/mas.c
+++ plugins/mas.c
@@ -849,6 +849,7 @@ static void get_message_cb(void *session, int err, gboolean fmore,
 	const char *chunk, void *user_data)
 {
 	struct mas_session *mas = user_data;
+	uint8_t fmore_byte;
 
 	DBG("");
 
@@ -859,6 +860,13 @@ static void get_message_cb(void *session, int err, gboolean fmore,
 
 	if (!chunk) {
 		mas->finished = TRUE;
+
+		if (aparams_read(mas->inparams, FRACTIONREQUEST_TAG, NULL)) {
+			fmore_byte = fmore ? 1 : 0;
+			aparams_write(mas->outparams, FRACTIONDELIVER_TAG,
+									&fmore);
+		}
+
 		goto proceed;
 	}
 
@@ -1021,6 +1029,9 @@ static void *message_open(const char *name, int oflag, mode_t mode,
 				void *driver_data, size_t *size, int *err)
 {
 	struct mas_session *mas = driver_data;
+	unsigned long flags;
+	uint8_t freq;
+	uint8_t charset = 0;
 
 	DBG("");
 
@@ -1030,6 +1041,16 @@ static void *message_open(const char *name, int oflag, mode_t mode,
 
 		return NULL;
 	}
+
+	if (aparams_read(mas->inparams, FRACTIONREQUEST_TAG, &freq)) {
+		flags |= MESSAGES_FRACTION;
+		if (freq & 0x01)
+			flags |= MESSAGES_NEXT;
+	}
+
+	aparams_read(mas->inparams, CHARSET_TAG, &charset);
+	if (charset & 0x01)
+		flags |= MESSAGES_UTF8;
 
 	*err = messages_get_message(mas->backend_data, name, 0,
 			get_message_cb, mas);
