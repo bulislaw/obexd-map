a81eb993364d902226bd7b876d8ed2cc56d5aeb1
Slawomir Bochenski <lkslawek@gmail.com>
2011-09-26 14:19:19 +0200

    XXX: MAP mem-leak note

--- plugins/mas.c
+++ plugins/mas.c
@@ -445,6 +445,7 @@ static GHashTable *parse_aparam(const uint8_t *buffer, uint32_t hlen)
 
 		switch (aparam_defs[tago].type) {
 		case APT_STR:
+			/* FIXME: mem leak? */
 			entry.valstr = g_try_malloc0(hdr->len + 1);
 			if (entry.valstr)
 				memcpy(entry.valstr, hdr->val, hdr->len);
