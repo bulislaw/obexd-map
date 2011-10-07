sragavan/obex

commit 934b3b2f5f8432b67822be9f83a141fe3783cbd7

Reverse order of calls in os_reset_session
This replaces:
service->get/put, mime->open, ..., service->reset, mime->close
logic, with a more appropriate:
service->get/put, mime->open, ..., mime->close, service->reset

src/obex.c
@@ -284,8 +284,6 @@ static void os_session_mark_aborted(struct obex_session *os)
 static void os_reset_session(struct obex_session *os)
 {
 	os_session_mark_aborted(os);
-	if (os->service && os->service->reset)
-		os->service->reset(os, os->service_data);
 
 	if (os->object) {
 		os->driver->set_io_watch(os->object, NULL, NULL);
@@ -296,6 +294,9 @@ static void os_reset_session(struct obex_session *os)
 				os->driver->remove)
 			os->driver->remove(os->path);
 	}
+
+	if (os->service && os->service->reset)
+		os->service->reset(os, os->service_data);
 
 	if (os->name) {
 		g_free(os->name);

bulislaw/obexd-map

commit 4ef8a39c2a5e10de780e81ee13264da179aad3b8

Fix OPP not sending TransferCompleted signal
Commit 934b3b2f5f8432b67822be9f83a141fe3783cbd7 introduced a regression
in OPP, by making it not send TransferCompleted signal. This happened
because OPP was calling manager_emit_transfer_completed() on .reset(),
and manager_emit_transfer_completed() in turn checks if the os->object
is not NULL. This fixes this by moving clearing os->object after doing
service driver .reset().

src/obex.c
@@ -287,8 +287,6 @@ static void os_reset_session(struct obex_session *os)
 	if (os->object) {
 		os->driver->set_io_watch(os->object, NULL, NULL);
 		os->driver->close(os->object);
-		os->object = NULL;
-		os->obj = NULL;
 		if (os->aborted && os->cmd == OBEX_CMD_PUT && os->path &&
 				os->driver->remove)
 			os->driver->remove(os->path);
@@ -314,6 +312,8 @@ static void os_reset_session(struct obex_session *os)
 		os->path = NULL;
 	}
 
+	os->object = NULL;
+	os->obj = NULL;
 	os->driver = NULL;
 	os->aborted = FALSE;
 	os->pending = 0;